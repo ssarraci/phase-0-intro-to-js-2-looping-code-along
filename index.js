848ce6314a2adb4c96daf10e3252f9d72d98e862
Storm <storm.sarracino01@gmail.com>
2021-05-22 16:36:08 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(name) {
+    let messages = [];
+    for (let i = 0; i < name.length; i++) {
+        messages.push(`Thank you, ${name[i]}, for the wonderful surprise gift!`);
+    }
+    return messages;
+}
+
+function countDown(number){
+    while (number >= 0){
+        console.log(number);
+        number--;
+    }
+    
+}
